b12a83adb26cead23d51da8b7a49e58c84104a6d
Stepan5024 <bokarevstepan1@yandex.ru>
2020-10-31 23:44:18 +0300

    html базис
    Добавлен html код для страницы ученика

--- testingSystem/urls.py
+++ testingSystem/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
 
-    path('', views.first)
+    path('', views.first),
+    path('classwork', views.classwork),
+    path('homework', views.homework),
+    path('examwork', views.examwork)
 ]
--- testingSystem/views.py
+++ testingSystem/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def classwork(request):
+    return render(request, 'testingSystem/student_classwork.html')
+
+
 def first(request):
     return render(request, 'testingSystem/student_first.html')
 
 
-def second(request):
-    return render(request, 'testingSystem/student_second.html')
+def homework(request):
+    return render(request, 'testingSystem/student_homework.html')
+
+def examwork(request):
+    return render(request, 'testingSystem/student_examwork.html')
